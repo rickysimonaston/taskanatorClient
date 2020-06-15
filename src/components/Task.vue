7ffca0f5a08656805acfdd2af92651cf73619d56
Ricky Aston <rickysimonaston@gmail.com>
2020-06-15 20:50:38 +1000

    Created Home page and task component

---
+++ src/components/Task.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="task">
+    <div class="columns is-multiline">
+      <div v-for="(task, index) in tasks" :key="index" class="column is-4">
+        <article class="message">
+          <div class="message-header">
+            <p>{{ task.title }}</p>
+            <button class="delete" aria-label="delete"></button>
+          </div>
+          <div class="message-body">
+            {{ task.body }}
+          </div>
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: {
+    tasks: Array,
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,46 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <section class="hero is-fullheight is-light is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title has-text-centered is-size-1">
+            Your Task Tracker
+          </h1>
+          <h2 class="subtitle has-text-centered is-size-4">
+            Your 3 latest Tasks
+          </h2>
+          <!-- Insert Task Component -->
+          <Task :tasks="tasks" />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Task from '@/components/Task';
 export default {
   name: 'Home',
+  data() {
+    return {
+      tasks: null,
+    };
+  },
+  components: {
+    Task,
+  },
+  methods: {
+    async getTasks() {
+      const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/todos'
+      );
+      const topTasks = response.data.slice(0, 3);
+      this.tasks = topTasks;
+    },
+  },
+  mounted() {
+    this.getTasks();
+  },
 };
 </script>
